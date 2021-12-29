fb50e50f1595a1301c7aababd07ac0e47e3a0f9f
Cameron Wills <68020747+CamWills89@users.noreply.github.com>
2021-12-29 16:40:24 -0500

    Updates

---
+++ precache-manifest.ba5f0c14c307532af2727fc442b7504d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259ddb1a2351971f3f0929f3fb0b3b4f",
+    "url": "/nick-profile/index.html"
+  },
+  {
+    "revision": "0d93519cf58edbc55390",
+    "url": "/nick-profile/static/css/main.569cc56b.chunk.css"
+  },
+  {
+    "revision": "4e36f4015fa54c2b8b9e",
+    "url": "/nick-profile/static/js/2.75779711.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nick-profile/static/js/2.75779711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d93519cf58edbc55390",
+    "url": "/nick-profile/static/js/main.3d0cb526.chunk.js"
+  },
+  {
+    "revision": "c8993c470ba23b0ef26d",
+    "url": "/nick-profile/static/js/runtime-main.17e43149.js"
+  },
+  {
+    "revision": "94fdd1486108974138599959531d00c9",
+    "url": "/nick-profile/static/media/self.94fdd148.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CamWills89/nick-profile/precache-manifest.54bf6b21ee3b42125021163a4d04043b.js"
+  "/nick-profile/precache-manifest.ba5f0c14c307532af2727fc442b7504d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CamWills89/nick-profile/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nick-profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
